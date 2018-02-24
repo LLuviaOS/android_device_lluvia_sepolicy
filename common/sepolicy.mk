2a8536fe747c63ccfb8883b997e4f711f05a4a16
Paul Keith <javelinanddart@gmail.com>
2018-02-24 21:39:42 +0100

    lluvia_sepolicy: Add sf_lcd_density_prop type and labelled props
    * Fixes sepolicy for recovery for devices that don't
      include device/qcom/sepolicy (like Nexus/Pixel)
    
    Change-Id: Iae1e15b524514ab30634b5e896603c43616faebc
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -2,6 +2,9 @@
 # This policy configuration will be used by all products that
 # inherit from Lineage
 #
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/lluvia/sepolicy/common/public
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/lluvia/sepolicy/common/private
 
