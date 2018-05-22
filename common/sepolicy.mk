Atomic-OS/device_aos_sepolicy

sepolicy: use correct flag for PRIVATE_SEPOLICY_DIR
Change-Id: I3c8ab7cb30902c3d64d1b180ed4c75e1cbb5681c
Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

common/sepolicy.mk
@@ -4,5 +4,7 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/aos/sepolicy/common/vendor \
+    device/aos/sepolicy/common/vendor
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aos/sepolicy/common/private

LLuviaOS/android_device_lluvia_sepolicy

Signed-off-by: leodiram <sriramscience7@gmail.com>

common/sepolicy.mk
@@ -2,7 +2,8 @@
 # This policy configuration will be used by all products that
 # inherit from Lineage
 #
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lluvia/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lluvia/sepolicy/common/vendor \
-    device/lluvia/sepolicy/common/private
+    device/lluvia/sepolicy/common/vendor
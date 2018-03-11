8771cae6fd8dba2f8b2f8895777b13a61b08bdd9
martinusbe <martinusbe@gmail.com>
2018-03-11 11:51:48 +0100

    sepolicy: use correct flag for PRIVATE_SEPOLICY_DIR
    Change-Id: I3c8ab7cb30902c3d64d1b180ed4c75e1cbb5681c
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -2,7 +2,8 @@
 # This policy configuration will be used by all products that
 # inherit from Lineage
 #
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lluvia/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lluvia/sepolicy/common/vendor \
-    device/lluvia/sepolicy/common/private
+    device/lluvia/sepolicy/common/vendor
