ArrowOS/android_device_arrow_sepolicy

sepolicy: Move LineageOS SDK policy to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/arrow/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/arrow/sepolicy/common/private

LLuviaOS/android_device_lluvia_sepolicy

Signed-off-by: Srijith Bharadwaj <srijithdamerla2001@gmail.com>

common/sepolicy.mk
@@ -4,4 +4,7 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/lluvia/sepolicy/common
+    device/lluvia/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lluvia/sepolicy/common/private