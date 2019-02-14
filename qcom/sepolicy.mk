95832c0440a5642147edb98f063842cf83e819e3
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 20:53:36 +0800

    sepolicy: qcom: Rename common to vendor to avoid confusion
     * Remove platform specific rule from sepolicy.mk as well, it's never
       ever a valid usecase.
    
    Change-Id: I8bf6af30407d55beb9b2c5a2afaca091564e1605
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,8 +1,7 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from AOSiP
+# that inherit from LLuvia
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/lluvia/sepolicy/qcom/common \
-    device/lluvia/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/lluvia/sepolicy/qcom/vendor
