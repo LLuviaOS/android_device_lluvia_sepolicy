AOSiP/device_aosip_sepolicy

sepolicy: update qcom/sepolicy.mk
Change-Id: Ib30abbc335657e72b5faafefe86db4dfe6c2c53e
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

qcom/sepolicy.mk
@@ -3,5 +3,20 @@
 # that inherit from AOSiP
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aosip/sepolicy/qcom/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aosip/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/aosip/sepolicy/qcom/dynamic \
     device/aosip/sepolicy/qcom/vendor
+endif

LLuviaOS/android_device_lluvia_sepolicy

Signed-off-by: Anush02198 <Anush.4376@gmail.com>

qcom/sepolicy.mk
@@ -3,5 +3,20 @@
 # that inherit from LLuvia
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lluvia/sepolicy/qcom/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/lluvia/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/lluvia/sepolicy/qcom/dynamic \
     device/lluvia/sepolicy/qcom/vendor
+endif